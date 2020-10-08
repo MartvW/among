0670a903dd0adbf1dbc21936b3cee88c71cfc595
MartvW <44231846+MartvW@users.noreply.github.com>
2020-10-08 19:09:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ bot.on('ready', async () => {
 bot.on('message', msg => {
     if (!msg.content.startsWith(prefix)) return;
     if (msg.author.bot) return;
-    if (msg.guild === null) {
+    if (msg.guild === null && msg.author.id != owner) {
         msg.reply("You can try it but it does not work in DM, trust me...");
         return;
     }
