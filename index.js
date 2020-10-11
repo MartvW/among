2e4f1204ad8327156c39bc43b6f4ca4762f5ef9d
MartvW <44231846+MartvW@users.noreply.github.com>
2020-10-11 19:31:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -129,17 +129,6 @@ bot.on('message', msg => {
             msg.channel.send(embedHelp);
             msg.channel.send(embedLetOp);
         }
-        
-        if (command === "reset") {
-            if (msg.author.id !== owner) return false;
-            var embed = new Discord.MessageEmbed()
-                .setTitle(`Among Us - @${msg.author.username}`)
-                .setDescription(`Bezig met resetten...`)
-                .setColor(16426522)
-            msg.reply(embed);
-            bot.destroy();
-            bot.login(token);
-        };
 
         if (command === "uptime") {
             let totalSeconds = (bot.uptime / 1000);
