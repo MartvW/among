5e2e224f48e5c29d18ce6a594a41ace34b2377c9
MartvW <44231846+MartvW@users.noreply.github.com>
2020-10-19 00:14:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -163,7 +163,6 @@ bot.on('ready', async () => {
 });
 
 bot.on('message', async msg => {
-    console.log(msg.guild);
     if (!msg.content.startsWith(prefix)) return;
     if (msg.author.bot) return;
     if (msg.guild === null && msg.author.id != owner) {
