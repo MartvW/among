2f47ae4ccba74652f1f7642eb42931f96aba90cf
MartvW <44231846+MartvW@users.noreply.github.com>
2020-10-18 18:13:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -53,14 +53,13 @@ const embedLetOp = {
 //ROOD: 15746887
 //ORANJE: 16426522
 
-function createEmbed(title, description, image) {
+function createEmbed(title, description) {
     var embed = new Discord.MessageEmbed()
         .setAuthor(`${bot.user.username}`, `https://cdn.discordapp.com/app-icons/469857906385354764/ea4f5a8c39e1b183777117bdd40a7449.png`)
         .setTitle(`${title}`)
         .setDescription(`${description}`)
         .setColor(16426522)
         .setTimestamp()
-        .setImage(`${image}`)
         .setFooter(`${bot.user.tag}`)
     return embed;
 }
@@ -247,14 +246,6 @@ bot.on('message', async msg => {
             msg.channel.send(createEmbed(`${msg.author.username}`, `Uptime: ${days} dagen, ${hours} uur, ${minutes} minuten en ${seconds} seconden.`));
         }
         
-        if (command === "map") {
-            msg.channel.send(createEmbed(`${msg.author.username}`, `Map:\nThe Skeld\nMira\nPolus\n\nDoe **${prefix}**<mapnaam> om de kaart van de map te zien!, ""));
-        }
-        
-        if (command === "skeld") {
-            msg.channel.send(createEmbed(`${msg.author.username}`, `Map: **The Skeld**\n`, `https://cdn.discordapp.com/attachments/763375592921235456/767396348818751498/latest.png`));
-        }
-
         if (command === "ping") {
             msg.channel.send("Pinging...").then(m => {
                 var ping = m.createdTimestamp - msg.createdTimestamp;
