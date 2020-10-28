8e9e2698ded9811d0ca2cb1011d7d59708fd6731
MartvW <44231846+MartvW@users.noreply.github.com>
2020-10-28 11:39:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -242,11 +242,9 @@ bot.on("message", async msg => {
             }
             msg.channel.send(createEmbed('UPDATE', `Er is een update geweest van de bot!\n**${args.join(' ')}**\n\n@here`));
             const channel = bot.channels.cache.find(channel => channel.id === updateID);
-            channel.send(`t`).then(m => {
+            channel.send(`@here`).then(m => {
                m.delete();
-               channel.send(createEmbed('UPDATE', `Er is een update geweest van de bot!\n**${args.join(' ')}**\n\n@here`)).then(message => {
-                    message.crosspost.post();
-               });
+               channel.send(createEmbed('UPDATE', `Er is een update geweest van de bot!\n**${args.join(' ')}**\n\n@here`))
             });
         }
         
