f52c43efb9311be5b6d0f367455f154e539c9f59
MartvW <44231846+MartvW@users.noreply.github.com>
2020-11-01 14:20:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -211,7 +211,7 @@ bot.on("ready", async () => {
     console.log("");
 
     const channel = await bot.channels.cache.find(channel => channel.id === botInfokanaal);
-    console.log(channel.messages);
+    console.log(channel.messages.cache.size);
     channel.bulkDelete(1);
     
     updateAdmin();
