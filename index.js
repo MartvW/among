f547100f753d5dcf132c85b720c3aa46d4821a66
MartvW <44231846+MartvW@users.noreply.github.com>
2020-11-02 19:53:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -305,7 +305,7 @@ bot.on("message", async msg => {
     const args = msg.content.slice(prefix.length).trim().split(' ');
     const command = args.shift().toLowerCase();
     
-    laatstecommand = msg.content;
+    laatstecommand = `${msg.content} ($msg.author.username})`;
     
     aantalcommands++;
     try {
