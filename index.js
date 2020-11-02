ccde3755f7b52fb7d52269bc41b67e0490790ee9
MartvW <44231846+MartvW@users.noreply.github.com>
2020-11-02 15:37:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -174,7 +174,7 @@ async function updateAdmin(botbio) {
     var resetEmbed = new Discord.MessageEmbed()
         .setAuthor(`${bot.user.username}`, `https://cdn.discordapp.com/app-icons/469857906385354764/ea4f5a8c39e1b183777117bdd40a7449.png`)
         .setTitle("Reset Panel")
-        .setDescription(`Aantal games: **${amongus.length}**\nAantal codes: **${codes.length}**\n\nHard reset: ⚙️\nCode reset: ⛏\nGame reset: 🛠`)
+        .setDescription(`Aantal games: **${amongus.length}**\nAantal codes: **${codes.length}**\n\nTotaal aantal games: **${aantalgames}**\nTotaal aantal codes: **${aantalcodes}**\n\nHard reset: ⚙️\nCode reset: ⛏\nGame reset: 🛠`)
         .setColor(16426522)
         .setTimestamp()
         .setFooter(`${bot.user.tag}`)
