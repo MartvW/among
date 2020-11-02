1aa644ac0d4d6adeb7f2058f6de7b64a270326d7
MartvW <44231846+MartvW@users.noreply.github.com>
2020-11-02 23:28:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ async function resetBot() {
     var resetEmbed = new Discord.MessageEmbed()
         .setAuthor(`${bot.user.username}`, `https://cdn.discordapp.com/app-icons/469857906385354764/ea4f5a8c39e1b183777117bdd40a7449.png`)
         .setTitle("Reset Panel")
-        .setDescription(`Slot: **${slotnaam}**\nAantal games: **${amongus.length}**\nAantal codes: **${codes.length}**\n\nHard reset: ⚙️\nCode reset: ⛏\nGame reset: 🛠\nNOODSTOP: 🔴`)
+        .setDescription(`Slot: **${slotnaam}**\nAantal games: **${amongus.length}**\nAantal codes: **${codes.length}**\n\nTotaal aantal games: **${aantalgames}**\nTotaal aantal codes: **${aantalcodes}**\n\nHard reset: ⚙️\nCode reset: ⛏\nGame reset: 🛠\nNOODSTOP: 🔴`)
         .setColor(16426522)
         .setTimestamp()
         .setFooter(`${bot.user.tag}`)
