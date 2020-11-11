4810faea5565915b18901aae68f3604c18b6efbd
MartvW <44231846+MartvW@users.noreply.github.com>
2020-11-11 09:09:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -449,9 +449,6 @@ bot.on("message", async msg => {
     if (slot === false) {
         aantalcommands++;
         try {
-            if (command === "race") {
-                msg.channel.send(createEmbed(`Race`, `Mart W. wins the race!`));   
-            }
             if (command === "help") {
                 msg.channel.send(helpEmbed());
                 msg.channel.send(embedLetOp);
