e32a8eb8b799c6358a984a75a69ee57ac140e8df
mweeghel <mweeghel3832@student.landstede.nl>
2020-11-17 11:30:06 +0100

    f

--- index.js
+++ index.js
@@ -457,7 +457,7 @@ bot.on("ready", async () => {
 
 bot.on("message", async msg => {
     let prefix = await client.query(`SELECT DISTINCT prefix FROM prefixes WHERE guildId='${msg.guild.id}';`);
-    console.log(prefix.rows.lastIndexOf(prefix));
+    console.log(prefix.rows.indexOf(prefix.length-1));
     if (!prefix) return console.log("Prefix niet gevonden");
 
     if (msg.author.bot) return;
