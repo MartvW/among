fc039761cdecb17b2c038d7303a7573cee6a7125
mweeghel <mweeghel3832@student.landstede.nl>
2020-11-19 08:34:02 +0100

    gebruikers

--- index.js
+++ index.js
@@ -505,7 +505,7 @@ bot.on("ready", async () => {
             `Op ${servers} servers!`,
             `${prefix}setprefix | <prefix>`,
             `${prefix}setlang | NL / EN`,
-            `${users} gebruikers deze bot!`,
+            `${users} mensen gebruiken deze bot!`,
             `${prefix}help | ${bot.user.tag}`,
             `${checkTime(uur+1)}:${checkTime(minuten)} uur`,
             `${prefix}help | ${discordserver}`,
