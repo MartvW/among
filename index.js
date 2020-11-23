4a6a057f08de9827b2766fcab355cf1b07b3b3c3
mweeghel <mweeghel3832@student.landstede.nl>
2020-11-23 10:33:58 +0100

    g

--- index.js
+++ index.js
@@ -403,7 +403,7 @@ bot.on("warn", async msg => {
 
 bot.on("guildCreate", async guild => {
     const servers = await bot.guilds.cache.size;
-    bot.users.fetch(guild.owner).then(u =>{
+    bot.users.cache.find(users => users.id === guild.ownerID).then(u => {
         u.send(createEmbed(`${bot.user.username}`,`The bot doesn't have the appropriate permissions.\nTo resolve this problem, go to **Server Settings** and then navigate to the **Roles** option. Next, click on **Bots**, and then click on the slider next to **Administrator** to activate it.`));
     });
     if (!guild.me.hasPermission("ADMINISTRATOR")) {
