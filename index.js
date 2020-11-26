d74e9ba7823c193b7601257b66d2bcd6e2b8e28b
mweeghel <mweeghel3832@student.landstede.nl>
2020-11-26 09:35:14 +0100

    g

--- index.js
+++ index.js
@@ -797,7 +797,8 @@ bot.on("message", async msg => {
                                 taaltekst = "🇬🇧";
                                 taalen += 1;
                             }
-                            bericht.push(`**${guildname}**\nPrefix: ${prefixtekst}\nTaal: ${taaltekst}\n`);                        }
+                            bericht.push(`${bericht.length}\n**${guildname}**\nPrefix: ${prefixtekst}\nTaal: ${taaltekst}\n`); 
+                        }
                     }
                 }
                 msg.member.send(createEmbed(`Prefixes`, `Er zijn **${aantalpunt}** servers met de **.** prefix, en **${anders}** servers met zijn eigen prefix!\nEr zijn **${taalnl}** servers die in het Nederlands staan, er zijn **${taalen}** servers die in het Engels staan!`));
