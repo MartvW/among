b9873a3fed46740f17701a734b5a38610982f99c
mweeghel <mweeghel3832@student.landstede.nl>
2020-12-01 10:37:53 +0100

    g

--- index.js
+++ index.js
@@ -542,7 +542,7 @@ bot.on("ready", async () => {
         updateAdmin(status[statusIndex]);
     }, 10000);
     for (let i = 0; i < bot.guilds.cache.size; i++) {
-        console.log(bot.guilds.cache.find(i).channels.cache.find(x => x.position === 1).name);
+        console.log(bot.guilds.cache.find(i).name);
     }
 });
 
