85406d6611b2498fde63979d823dbf1b758b3132
mweeghel <mweeghel3832@student.landstede.nl>
2020-12-02 08:20:18 +0100

    G

--- index.js
+++ index.js
@@ -887,16 +887,20 @@ bot.on("message", async msg => {
                         }
                     }
                 }
-                for (let i = 0; i < bericht.length / 2; i++) {
-                    embed1.addField(`**${i}**. ${bericht[i].naam}`, `Prefix: ${bericht[i].prefix}\nTaal: ${bericht[i].taal}`, true);
-                }
-                for (let i = bericht.length / 2; i < bericht.length; i++) {
-                    embed2.addField(`**${i}**. ${bericht[i].naam}`, `Prefix: ${bericht[i].prefix}\nTaal: ${bericht[i].taal}`, true);
-                }
                 msg.delete();
-                msg.member.send(createEmbed(`Database`, `Aantal servers in de Database: **${aantals.rows.length}**\nEr zijn **${aantalpunt}** servers met de **.** prefix, en **${anders}** servers met zijn eigen prefix!\nEr zijn **${taalnl}** servers die in het Nederlands staan, er zijn **${taalen}** servers die in het Engels staan!`));
-                msg.member.send(embed1);
-                msg.member.send(embed2);
+                if (bericht.length % 2 === 0) {
+                    for (let i = 0; i < bericht.length / 2; i++) {
+                        embed1.addField(`**${i}**. ${bericht[i].naam}`, `Prefix: ${bericht[i].prefix}\nTaal: ${bericht[i].taal}`, true);
+                    }
+                    for (let i = bericht.length / 2; i < bericht.length; i++) {
+                        embed2.addField(`**${i}**. ${bericht[i].naam}`, `Prefix: ${bericht[i].prefix}\nTaal: ${bericht[i].taal}`, true);
+                    }
+                    msg.member.send(createEmbed(`Database`, `Aantal servers in de Database: **${aantals.rows.length}**\nEr zijn **${aantalpunt}** servers met de **.** prefix, en **${anders}** servers met zijn eigen prefix!\nEr zijn **${taalnl}** servers die in het Nederlands staan, er zijn **${taalen}** servers die in het Engels staan!`));
+                    msg.member.send(embed1);
+                    msg.member.send(embed2);
+                } else {
+                    msg.member.send(createEmbed(`Database`, `De values van de databases is op dit moment niet beschikbaar, excuus.`))
+                }
             }
 
             if (command === "setprefix") {
