e7d768e8fc23f56f5860434f85f37fe730ef2c9d
mweeghel <mweeghel3832@student.landstede.nl>
2020-12-03 10:39:47 +0100

    g

--- index.js
+++ index.js
@@ -267,6 +267,7 @@ async function updateAdmin(botbio) {
     }   
 
     console.log(prefixesArray);
+    let prefixes = prefixesArray.join(", ");
 
     var adminEmbed = new Discord.MessageEmbed()
         .setAuthor(`${bot.user.username}`, `https://cdn.discordapp.com/app-icons/469857906385354764/ea4f5a8c39e1b183777117bdd40a7449.png`)
