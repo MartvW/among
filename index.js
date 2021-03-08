9a54d1262ff06b6b334cf1d8e013382ea736b155
MartvW <44231846+MartvW@users.noreply.github.com>
2021-03-08 12:00:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function helpEmbed(prefixs, lang, kleurVar) {
         var embedHelp = new Discord.MessageEmbed()
             .setAuthor(`SusBot`, `https://raw.githubusercontent.com/MartvW/among/master/Logo.png`)
             .setTitle("Help")
-            .setDescription(`Hier is een lijstje met de commands die je kan gebruiken.\nOm alle commands op een rij te zien ga naar http://susbot.epizy.com/command.html`)
+            .setDescription(`Hier is een lijstje met de commands die je kan gebruiken.`)
             .addFields(
                 { name: `${prefixs}setprefix`, value: 'Hiermee kan je je eigen prefix instellen.', inline: true },
                 { name: `${prefixs}setlang`, value: 'Hiermee kan je je eigen taal instellen.', inline: true },
@@ -82,7 +82,7 @@ function helpEmbed(prefixs, lang, kleurVar) {
         var embedHelp = new Discord.MessageEmbed()
             .setAuthor(`SusBot`, `https://raw.githubusercontent.com/MartvW/among/master/Logo.png`)
             .setTitle("Help")
-            .setDescription(`A list of all the commands I know.\nTo see all the commands on a list, go to http://susbot.epizy.com/command.html`)
+            .setDescription(`A list of all the commands I know.`)
             .addFields(
                 { name: `${prefixs}setprefix`, value: 'To set your own prefix.', inline: true },
                 { name: `${prefixs}setlang`, value: 'To set your own language.', inline: true },
@@ -485,7 +485,7 @@ bot.on("guildCreate", async guild => {
     }
 
     if (guild.systemChannel) {
-        guild.systemChannel.send(createEmbed(`${bot.user.username}`, `Thanks for adding me to this server!\nYou can find all my commands by typing **${prefix}help** or go to http://susbot.epizy.com/command.html\nYou can set your own prefix by typing **${prefix}setprefix**\nSet your own color by typing **${prefix}setcolor**\n\nFor people who wants to invite me, type **${prefix}link** to get the invite-link!\n\nI have a Patreon Page where you can donate money: https://www.patreon.com/bePatron?u=45897916`, '#FFAC33'));
+        guild.systemChannel.send(createEmbed(`${bot.user.username}`, `Thanks for adding me to this server!\nYou can find all my commands by typing **${prefix}help**\nYou can set your own prefix by typing **${prefix}setprefix**\nSet your own color by typing **${prefix}setcolor**\n\nFor people who wants to invite me, type **${prefix}link** to get the invite-link!\n`, '#FFAC33'));
         guild.systemChannel.send(createEmbed(`Language`, `If you want to set the bot in another language, do ***${prefix}setlang***!`, '#FFAC33'));
     }
 });
@@ -567,7 +567,6 @@ bot.on("ready", async () => {
             `${prefix}setlang | Custom Language!`,
             `${prefix}setcolor | Your own color!`,
             `${users} are using this bot!`,
-            `https://www.patreon.com/bePatron?u=45897916`,
             `${prefix}help | ${bot.user.tag}`,
             `Made by Mart W.#0160`,
             `${prefix}amongus | Start command!`,
@@ -1585,9 +1584,7 @@ bot.on("message", async msg => {
                         .addFields(
                             { name: `Invite link bot`, value: 'https://discord.com/oauth2/authorize?client_id=469857906385354764&scope=bot&permissions=8', inline: false },
                             { name: `Top.gg link`, value: `https://top.gg/bot/469857906385354764`, inline: false },
-                            { name: `Patreon link`, value: `https://www.patreon.com/bePatron?u=45897916`, inline: false },
                             { name: `Discord link`, value: `${discordserver}`, inline: false },
-                            { name: `Website link`, value: `http://susbot.epizy.com/`, inline: false },
                         )
                         .setColor(kleur)
                         .setTimestamp()
@@ -1600,9 +1597,7 @@ bot.on("message", async msg => {
                         .addFields(
                             { name: `Invite link bot`, value: 'https://discord.com/oauth2/authorize?client_id=469857906385354764&scope=bot&permissions=8', inline: false },
                             { name: `Top.gg link`, value: `https://top.gg/bot/469857906385354764`, inline: false },
-                            { name: `Patreon link`, value: `https://www.patreon.com/bePatron?u=45897916`, inline: false },
                             { name: `Discord link`, value: `${discordserver}`, inline: false },
-                            { name: `Website link`, value: `http://susbot.epizy.com/`, inline: false },
                         )
                         .setColor(kleur)
                         .setTimestamp()
